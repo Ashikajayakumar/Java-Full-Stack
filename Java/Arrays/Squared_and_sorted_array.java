fd2c34bbd51080e4e3721d648592c3d9bc9f22fa
Ashika341 <ashikajayakumar26@gmail.com>
2025-07-10 22:26:10 +0530

    Create Squared_and_sorted_array.java

---
+++ Java/Arrays/Squared_and_sorted_array.java
@@ -0,0 +1,48 @@
+✅  Java Solution – Bubble sort Approach
+  
+  class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) { 
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
+
+
+
+
+
+
+✅ Optimal Java Solution – Two Pointer Approach
+  
+  class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1; 
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            if (leftSquare > rightSquare) {
+                result[index--] = leftSquare;
+                left++;
+            } else {
+                result[index--] = rightSquare;
+                right--;
+            }
+        }
+        return result;
+    }
+}
